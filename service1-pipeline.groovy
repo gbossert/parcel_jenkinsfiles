49691f42face099951bce8955b845b5dfd253fae
Georges Bossert <georges.bossert@inthreat.com>
2018-01-24 17:24:21 +0100

    wip

--- service1-pipeline.groovy
+++ service1-pipeline.groovy
@@ -1,9 +1,65 @@
 #!groovy
 
-// def build() {
-//     println("Building service1")
-// }
+stage('Build Service1') {
+    when {
+	expression {
+	    return params.targets == 'all' || params.targets.contains('build')
+	}
+    }
+    
+    parallel {
+	stage('Build Service1/API') {
+	    agent {
+		label 'python'
+	    }
+	    when {
+		expression {
+		    return params.microservices == 'all' || params.microservices.contains('service1api')
+		}
+	    }
+	    steps {
+		script {
+		    sh 'building service1-api'
+		    // service1_pipeline = load './service1-pipeline.groovy'
+		    // service1_pipeline.build()
 
-// return this
+		    
+		}
+	    }
+	}
+	stage('Build Service1/Worker') {
+	    agent {
+		label 'python'
+	    }		    
+	    when {
+		expression {
+		    return params.microservices == 'all' || params.microservices.contains('service1worker')
+		}
+	    }
+	    steps {
+		sh "echo 'building service1/worker'"
+	    }
+	}
+	stage('Build Service1/Front') {
+	    agent {
+		label 'nodejs'
+	    }
+	    when {
+		expression {
+		    return params.microservices == 'all' || params.microservices.contains('service1front')
+		}
+	    }
+	    steps {
+		sh "echo 'building service1/front'"
+	    }
+	}
+    }	    
+}
 
-sh 'echo "plop plop"'
+// // def build() {
+// //     println("Building service1")
+// // }
+
+// // return this
+
+// sh 'echo "plop plop"'
