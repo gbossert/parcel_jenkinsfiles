a4cdbd4daeeb204ea4eec23040f5f6b81ec0918f
Georges Bossert <georges.bossert@inthreat.com>
2018-01-24 17:33:28 +0100

    wip

--- service1-pipeline.groovy
+++ service1-pipeline.groovy
@@ -1,62 +1,58 @@
 #!groovy
-pipeline {
-    stages {
-	stage('Build Service1') {
-	    when {
-		expression {
-		    return params.targets == 'all' || params.targets.contains('build')
+stage('Build Service1') {
+    // when {
+    // 	expression {
+    // 	    return params.targets == 'all' || params.targets.contains('build')
+    // 	}
+    // }
+    
+    parallel {
+	stage('Build Service1/API') {
+	    agent {
+		label 'python'
+	    }
+	    // when {
+	    // 	expression {
+	    // 	    return params.microservices == 'all' || params.microservices.contains('service1api')
+	    // 	}
+	    // }
+	    steps {
+		script {
+		    sh 'building service1-api'
+		    // service1_pipeline = load './service1-pipeline.groovy'
+		    // service1_pipeline.build()
+
+		    
 		}
 	    }
-	    
-	    parallel {
-		stage('Build Service1/API') {
-		    agent {
-			label 'python'
-		    }
-		    when {
-			expression {
-			    return params.microservices == 'all' || params.microservices.contains('service1api')
-			}
-		    }
-		    steps {
-			script {
-			    sh 'building service1-api'
-			    // service1_pipeline = load './service1-pipeline.groovy'
-			    // service1_pipeline.build()
-
-			    
-			}
-		    }
-		}
-		stage('Build Service1/Worker') {
-		    agent {
-			label 'python'
-		    }		    
-		    when {
-			expression {
-			    return params.microservices == 'all' || params.microservices.contains('service1worker')
-			}
-		    }
-		    steps {
-			sh "echo 'building service1/worker'"
-		    }
-		}
-		stage('Build Service1/Front') {
-		    agent {
-			label 'nodejs'
-		    }
-		    when {
-			expression {
-			    return params.microservices == 'all' || params.microservices.contains('service1front')
-			}
-		    }
-		    steps {
-			sh "echo 'building service1/front'"
-		    }
-		}
-	    }	    
 	}
-    }
+	stage('Build Service1/Worker') {
+	    agent {
+		label 'python'
+	    }		    
+	    // when {
+	    // 	expression {
+	    // 	    return params.microservices == 'all' || params.microservices.contains('service1worker')
+	    // 	}
+	    // }
+	    steps {
+		sh "echo 'building service1/worker'"
+	    }
+	}
+	stage('Build Service1/Front') {
+	    agent {
+		label 'nodejs'
+	    }
+	    // when {
+	    // 	expression {
+	    // 	    return params.microservices == 'all' || params.microservices.contains('service1front')
+	    // 	}
+	    // }
+	    steps {
+		sh "echo 'building service1/front'"
+	    }
+	}
+    }	    
 }
 // // def build() {
 // //     println("Building service1")
